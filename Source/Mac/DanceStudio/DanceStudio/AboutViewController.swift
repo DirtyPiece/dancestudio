c4355dad8950a8e4eaa879cd5366551977d0bf62
Dirty Piece <dirtypiece@Clarks-iMac.local>
2015-01-03 11:54:28 -0800

    Adding in the About box for Dance Studio.  Also started working on setting up some storyboard stuff.

---
+++ Source/Mac/DanceStudio/DanceStudio/AboutViewController.swift
@@ -0,0 +1,34 @@
+// =======================================================================
+// <copyright file="AboutViewController.swift" company="Bean &amp; Cheese Studios">
+// Copyright 2015 Bean and Cheese Studios
+// </copyright>
+// <date>01-03-2015</date>
+// =======================================================================
+
+import Cocoa
+
+/// <summary>
+/// Represents the about box view controller for the Dance Studio window.
+/// </summary>
+class AboutViewController: NSViewController {
+    /// <summary>
+    /// Called when the view has finished loading.
+    /// </summary>
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        // Do any additional setup after loading the view.
+    }
+    
+    /// <summary>
+    /// Property hook for determining when the represented object was set.
+    /// </summary>
+    override var representedObject: AnyObject? {
+        didSet {
+            // Update the view, if already loaded.
+        }
+    }
+}
+
+
+
