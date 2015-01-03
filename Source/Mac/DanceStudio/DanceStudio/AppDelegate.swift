ecc9c21bab6a66840eb2561570f91d9804264275
Dirty Piece <dirtypiece@Clarks-iMac.local>
2015-01-03 09:19:31 -0800

    Initial Commit

---
+++ Source/Mac/DanceStudio/DanceStudio/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  DanceStudio
+//
+//  Created by Dirty Piece on 1/3/15.
+//  Copyright (c) 2015 Bean & Cheese Studios. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(aNotification: NSNotification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(aNotification: NSNotification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ Source/Mac/DanceStudio/DanceStudio/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  DanceStudio
+//
+//  Created by Dirty Piece on 1/3/15.
+//  Copyright (c) 2015 Bean & Cheese Studios. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: AnyObject? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
